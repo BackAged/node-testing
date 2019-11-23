38f45bee1fd526104f0f6088342aa1ad27b852d9
BackAged <brotherapes@gmail.com>
2019-11-24 01:47:46 +0600

    Add spying

--- addController.test.js
+++ addController.test.js
@@ -8,12 +8,17 @@ describe('add', () => {
   const addC = new addController(mAdder);
 
   it('should add two positive numbers', () => {
+    // stubbing
     adder.mock.instances[0].add.mockClear();
     adder.mock.instances[0].add.mockImplementationOnce(
       (a,b) => a + b
     )
 
     expect( addC.add(2, 3) ).toBe(5);
+
+    //spying
+    expect( adder.mock.instances[0].add).toHaveBeenCalledTimes(1);
+    expect( adder.mock.instances[0].add).toHaveBeenCalledWith(2, 3);
   });
 
   it('should throw error for negative numbers', () => {
@@ -25,5 +30,9 @@ describe('add', () => {
     );
 
     expect(() => addC.add(2,-3)).toThrow();
+
+    //spying
+    expect( adder.mock.instances[0].add).toHaveBeenCalledTimes(1);
+    expect( adder.mock.instances[0].add).toHaveBeenCalledWith(2, -3);
   });
 });
